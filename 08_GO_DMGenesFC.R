bddb4cc2411f28936659d6b7e1a1703645c0dac2
leafaivre <leafaivre@fu-berlin.de>
2023-10-19 18:29:17 +0200

    GO study

--- 08_GO_DMGenesFC.R
+++ 08_GO_DMGenesFC.R
@@ -24,7 +24,7 @@ Araport <- TxDb.Athaliana.BioMart.plantsmart28
 Genes <- GenomicFeatures::genes(Araport,columns = c("GENEID","TXTYPE"))
 Genesdf <- as.data.frame(Genes)
 
-# GO analysis -------------------------------------------------------------
+# GO analysis biological function -------------------------------------------------------------
 
 #Making a vector of interesting genes: all genes, with 0 for non DM and 1 for DM
 GeneList <- lapply(DMGenes, function(x){
@@ -55,7 +55,7 @@ FormattedTable <- lapply(GOTable, function(x){
 
 
 for (i in names(FormattedTable)) {
-  write.xlsx(x = as.data.frame(FormattedTable[[i]]),
+  #write.xlsx(x = as.data.frame(FormattedTable[[i]]),
              file = "Results/GO_DMGenes_FC_0.01.xlsx",
              sheetName = paste(i, sep = ""),
              row.names = FALSE,
@@ -67,35 +67,28 @@ for (i in names(FormattedTable)) {
 
 allGO <- genesInTerm(GOTest$K4_3h_Gain)
 
-
-  lapply(GOTest, genesInTerm)
-
 Interesting <- lapply(DMGenes, function(x) {
   x %>%
-    filter(Gene %in% unlist(allGO["GO:0009266"])) 
+    filter(Gene %in% unlist(allGO["GO:0090693"])) 
 })
-
 
 # Graphs ------------------------------------------------------------------
 
 Top20 <- lapply(FormattedTable, function(x){
   x %>%
-    filter(Significant >= 2) %>%
     filter(FoldEnrichment > 1) %>%
     slice_max(logPvalue, n = 20, with_ties = FALSE) %>%
     mutate(GOTerm = paste(Term, "(", GO.ID, ")"))
-  
-  
 })
 
 ggplot(Top20$K4_3h_Gain, aes(x = reorder(GOTerm, logPvalue), y = logPvalue, 
                            fill = reorder(GOTerm, -logPvalue))) +
   geom_col() +
   coord_flip() +
-  scale_fill_manual(values = c("skyblue", "dimgray", "dimgray","skyblue", "royalblue4",
-                               "dimgray", "skyblue", "dimgray","coral3", "skyblue",
-                               "dimgray", "royalblue4", "dimgray","skyblue", "dimgray",
-                               "skyblue", "dimgray", "skyblue","dimgray", "plum4")) +
+  scale_fill_manual(values = c("royalblue4", "skyblue", "dimgray","skyblue", "dimgray",
+                               "skyblue", "skyblue", "dimgray","skyblue", "dimgray",
+                               "plum4", "skyblue", "dimgray","dimgray", "skyblue",
+                               "skyblue", "coral3", "coral3","skyblue", "skyblue")) +
   labs(x = "", y = "log10(1/P-value)", title = "Genes gaining H3K4me3 after 3h of cold exposure") +
   theme(axis.text = element_text(colour = "black"),
         legend.position = "none")
@@ -105,23 +98,22 @@ ggplot(Top20$K4_3h_Loss, aes(x = reorder(GOTerm, logPvalue), y = logPvalue,
                              fill = reorder(GOTerm, -logPvalue))) +
   geom_col() +
   coord_flip() +
-  scale_fill_manual(values = c("dimgray", "darkseagreen", "plum4", "dimgray", "dimgray",
-                               "darkseagreen", "coral3", "skyblue", "plum4", "dimgray",
-                               "royalblue4", "skyblue", "dimgray", "dimgray", "darkseagreen",
-                               "dimgray", "dimgray", "plum4", "dimgray", "dimgray")) +
+  scale_fill_manual(values = c("dimgray", "dimgray", "dimgray", "dimgray", "turquoise4",
+                               "darkgoldenrod4", "dimgray", "turquoise4", "turquoise4", "dimgray",
+                               "darkgoldenrod4", "darkgoldenrod4")) +
   labs(x = "", y = "log10(1/P-value)", title = "Genes losing H3K4me3 after 3h of cold exposure") +
   theme(axis.text = element_text(colour = "black"),
         legend.position = "none")
-#ggsave("Plots/GO/3h_K4_Loss.tiff", units = "in", width = 8, height = 4, dpi = 300, compression = 'lzw')
+#ggsave("Plots/GO/3h_K4_Loss.tiff", units = "in", width = 8, height = 3, dpi = 300, compression = 'lzw')
 
 ggplot(Top20$K4_3d_Gain, aes(x = reorder(GOTerm, logPvalue), y = logPvalue, 
                              fill = reorder(GOTerm, -logPvalue))) +
   geom_col() +
   coord_flip() +
-  scale_fill_manual(values = c("dimgray", "dimgray", "royalblue4", "skyblue", "plum4",
-                               "skyblue", "skyblue", "skyblue", "skyblue", "dimgray",
-                               "royalblue4", "skyblue", "dimgray", "royalblue4", "skyblue",
-                               "coral3", "plum4", "skyblue", "dimgray", "coral3")) +
+  scale_fill_manual(values = c("skyblue", "royalblue4", "dimgray", "skyblue", "skyblue",
+                               "dimgray", "skyblue", "skyblue", "skyblue", "skyblue",
+                               "royalblue4", "skyblue", "skyblue", "skyblue", "skyblue",
+                               "royalblue4", "skyblue", "dimgray", "dimgray", "plum4")) +
   labs(x = "", y = "log10(1/P-value)", title = "Genes gaining H3K4me3 after 3d of cold exposure") +
   theme(axis.text = element_text(colour = "black"),
         legend.position = "none")
@@ -131,10 +123,10 @@ ggplot(Top20$K4_3d_Loss, aes(x = reorder(GOTerm, logPvalue), y = logPvalue,
                              fill = reorder(GOTerm, -logPvalue))) +
   geom_col() +
   coord_flip() +
-  scale_fill_manual(values = c("coral3", "darkseagreen", "coral3", "dimgray", "skyblue",
-                               "royalblue4", "goldenrod3", "skyblue", "coral3", "dimgray",
-                               "darkseagreen", "skyblue", "dimgray", "coral3", "skyblue",
-                               "goldenrod3", "dimgray", "darkseagreen", "dimgray", "skyblue")) +
+  scale_fill_manual(values = c("dimgray", "coral3", "skyblue", "dimgray", "darkseagreen",
+                               "dimgray", "dimgray", "darkseagreen", "skyblue", "dimgray",
+                               "dimgray", "dimgray", "dimgray", "skyblue", "coral3",
+                               "dimgray", "dimgray", "dimgray", "dimgray", "darkseagreen")) +
   labs(x = "", y = "log10(1/P-value)", title = "Genes losing H3K4me3 after 3d of cold exposure") +
   theme(axis.text = element_text(colour = "black"),
         legend.position = "none")
@@ -144,10 +136,10 @@ ggplot(Top20$K27_3h_Gain, aes(x = reorder(GOTerm, logPvalue), y = logPvalue,
                              fill = reorder(GOTerm, -logPvalue))) +
   geom_col() +
   coord_flip() +
-  scale_fill_manual(values = c("plum4", "dimgray", "dimgray", "plum4", "plum4",
-                               "dimgray", "dimgray", "darkseagreen", "darkseagreen", "darkseagreen",
-                               "darkseagreen", "darkseagreen", "darkseagreen", "darkseagreen", "darkseagreen",
-                               "goldenrod3", "dimgray", "darkseagreen", "dimgray", "dimgray")) +
+  scale_fill_manual(values = c("dimgray", "dimgray", "darkseagreen", "darkseagreen", "darkseagreen",
+                               "darkseagreen", "darkseagreen", "dimgray", "dimgray", "darkseagreen",
+                               "darkseagreen", "plum4", "dimgray", "darkseagreen", "skyblue",
+                               "darkseagreen", "dimgray", "dimgray", "dimgray", "dimgray")) +
   labs(x = "", y = "log10(1/P-value)", title = "Genes gaining H3K27me3 after 3h of cold exposure") +
   theme(axis.text = element_text(colour = "black"),
         legend.position = "none")
@@ -157,34 +149,31 @@ ggplot(Top20$K27_3h_Loss, aes(x = reorder(GOTerm, logPvalue), y = logPvalue,
                               fill = reorder(GOTerm, -logPvalue))) +
   geom_col() +
   coord_flip() +
-  scale_fill_manual(values = c("dimgray", "dimgray", "dimgray", "dimgray", "goldenrod3",
-                               "dimgray", "dimgray", "plum4", "skyblue", "dimgray", "dimgray")) +
+  scale_fill_manual(values = c("skyblue", "dimgray", "skyblue", "dimgray", "dimgray")) +
   labs(x = "", y = "log10(1/P-value)", title = "Genes losing H3K27me3 after 3h of cold exposure") +
   theme(axis.text = element_text(colour = "black"),
         legend.position = "none")
-#ggsave("Plots/GO/3h_K27_Loss.tiff", units = "in", width = 8, height = 2.8, dpi = 300, compression = 'lzw')
+#ggsave("Plots/GO/3h_K27_Loss.tiff", units = "in", width = 8, height = 1.8, dpi = 300, compression = 'lzw')
 
 ggplot(Top20$K27_3d_Gain, aes(x = reorder(GOTerm, logPvalue), y = logPvalue, 
                               fill = reorder(GOTerm, -logPvalue))) +
   geom_col() +
   coord_flip() +
-  scale_fill_manual(values = c("plum4", "plum4", "dimgray", "plum4", "dimgray",
-                               "darkseagreen", "dimgray", "dimgray", "darkseagreen", "dimgray",
-                               "darkseagreen", "dimgray", "dimgray", "goldenrod3", "darkseagreen",
-                               "darkseagreen", "dimgray", "darkseagreen", "skyblue", "darkseagreen")) +
+  scale_fill_manual(values = c("dimgray", "dimgray", "darkseagreen", "darkseagreen", "darkseagreen",
+                               "darkseagreen", "darkseagreen", "darkseagreen", "darkseagreen", "plum4",
+                               "darkseagreen", "darkseagreen", "darkseagreen", "darkseagreen", "dimgray",
+                               "skyblue")) +
   labs(x = "", y = "log10(1/P-value)", title = "Genes gaining H3K27me3 after 3d of cold exposure") +
   theme(axis.text = element_text(colour = "black"),
         legend.position = "none")
-#ggsave("Plots/GO/3d_K27_Gain.tiff", units = "in", width = 8, height = 4, dpi = 300, compression = 'lzw')
+#ggsave("Plots/GO/3d_K27_Gain.tiff", units = "in", width = 8, height = 3.3, dpi = 300, compression = 'lzw')
 
 ggplot(Top20$K27_3d_Loss, aes(x = reorder(GOTerm, logPvalue), y = logPvalue, 
                               fill = reorder(GOTerm, -logPvalue))) +
   geom_col() +
   coord_flip() +
-  scale_fill_manual(values = c("dimgray", "dimgray", "dimgray", "dimgray", "dimgray",
-                               "dimgray", "dimgray", "skyblue", "dimgray", "dimgray",
-                               "dimgray", "dimgray", "skyblue", "skyblue", "dimgray")) +
+  scale_fill_manual(values = c("dimgray", "darkseagreen")) +
   labs(x = "", y = "log10(1/P-value)", title = "Genes losing H3K27me3 after 3d of cold exposure") +
   theme(axis.text = element_text(colour = "black"),
         legend.position = "none")
-ggsave("Plots/GO/3d_K27_Loss.tiff", units = "in", width = 8, height = 3, dpi = 300, compression = 'lzw')
+#ggsave("Plots/GO/3d_K27_Loss.tiff", units = "in", width = 8, height = 1.2, dpi = 300, compression = 'lzw')
